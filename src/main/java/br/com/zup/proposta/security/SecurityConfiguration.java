dfe2c993094ed624dc680becd86502458d8f210f
lexipediaa <alexsander.viana@zup.com.br>
2021-07-27 20:14:59 -0300

    Implementação keycloak

---
+++ src/main/java/br/com/zup/proposta/security/SecurityConfiguration.java
@@ -0,0 +1,23 @@
+package br.com.zup.proposta.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests -> authorizeRequests
+                .antMatchers(HttpMethod.GET, "/proposta/**").hasAuthority("SCOPE_proposta-read")
+                .antMatchers(HttpMethod.POST, "/proposta").hasAuthority("SCOPE_proposta-write")
+                    .anyRequest().authenticated()
+        ).oauth2ResourceServer(
+                OAuth2ResourceServerConfigurer::jwt
+        );
+
+    }
+}
